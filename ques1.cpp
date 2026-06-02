0bdf8860ae42096c43357f6d62ddfbe0c41e2d4e
ayushsahu-07 <ayushsahu9807@gmail.com>
2026-06-02 13:26:27 +0530

    Add files via upload

---
+++ ques1.cpp
@@ -0,0 +1,16 @@
+//program to calculate sum of first n natural number
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,sum = 0;
+    cout<<"Enter Nth number";
+    cin>>n;
+    for (i = 1; i <=n; i++)
+    {
+        sum+=i;
+    }
+    cout<<"sum of "<<n<<"numbers:"<<sum;
+    return 0;    
+}   
